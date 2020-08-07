85dc7ff44f8a17e25693fc3cc055eb21b059cfd8
James Roller, Jr <james.roller@aspencapital.com>
2020-08-07 13:11:28 -0700

    feat: add /users/me endpoint with automatic authentication

--- demo/graph-tutorial/routes/auth.js
+++ demo/graph-tutorial/routes/auth.js
@@ -7,32 +7,55 @@ var router = express.Router();
 
 /* GET auth callback. */
 router.get('/signin',
-  function  (req, res, next) {
+  function (req, res, next) {
     passport.authenticate('azuread-openidconnect',
       {
         response: res,
         prompt: 'login',
         failureRedirect: '/',
         failureFlash: true,
-        successRedirect: '/'
+        successRedirect: '/',
+        customState: Buffer.from(req.originalUrl).toString('base64'),
       }
     )(req,res,next);
   }
 );
 
-// <CallbackRouteSnippet>
-router.post('/callback',
-  function(req, res, next) {
-    passport.authenticate('azuread-openidconnect',
+function authCallback(opts={}) {
+  const defaultOpts = {
+    failureFlash: true,
+    failureRedirect: '/',
+    provider: 'azuread-openidconnect',
+    successRedirect: '/',
+  };
+  opts = Object.assign({}, defaultOpts, opts);
+
+  return (req, res, next) => {
+    passport.authenticate(opts.provider,
       {
         response: res,
-        failureRedirect: '/',
-        failureFlash: true,
-        successRedirect: '/'
+        failureRedirect: opts.failureRedirect,
+        failureFlash: opts.failureFlash,
+        // successRedirect: opts.successRedirect,
+        customState: Buffer.from(req.originalUrl).toString('base64'),
       }
-    )(req,res,next);
+    )(req, res, next);
   }
-);
+}
+
+function extractState(req, res, next) {
+  if (req.isAuthenticated()) {
+    const { state } = req.body;
+    const redirect = state ? new Buffer(state, 'base64').toString() : undefined;
+    if (typeof redirect === 'string' && redirect.startsWith('/')) {
+      return res.redirect(redirect);
+    }
+  }
+  next();
+}
+
+// <CallbackRouteSnippet>
+router.post('/callback', authCallback(), extractState);
 // </CallbackRouteSnippet>
 
 router.get('/signout',
--- demo/graph-tutorial/routes/users.js
+++ demo/graph-tutorial/routes/users.js
@@ -1,9 +1,38 @@
+const passport = require('passport');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+function authenticate(opts={}) {
+  const defaultOpts = {
+    passThru: true,
+    provider: 'azuread-openidconnect',
+  };
+  opts = Object.assign({}, defaultOpts, opts);
+
+  return (req, res, next) => {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      passport.authenticate(opts.provider, {
+        response: res,
+        customState: Buffer.from(req.originalUrl).toString('base64'),
+      })(req, res, next);
+    }
+  }
+}
+
+// References for dynamic redirects after authentication
+// https://stackoverflow.com/questions/47793615/utilizing-state-customstate-with-passport-azure-ad
+// https://dev.to/dangolant/dynamic-auth-redirects-with-passportjs-380g
+
+router.get('/me', authenticate(), (req, res) => {
+  res.send('me!!!');
 });
 
 module.exports = router;

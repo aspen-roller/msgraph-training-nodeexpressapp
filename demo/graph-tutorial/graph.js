e99dda39c716b069fc0b80a0b765d477f9d9722b
James Roller, Jr <james.roller@aspencapital.com>
2020-08-07 17:58:19 -0700

    feat: add graph.getUserRole()

--- demo/graph-tutorial/graph.js
+++ demo/graph-tutorial/graph.js
@@ -1,32 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-var graph = require('@microsoft/microsoft-graph-client');
+const graph = require('@microsoft/microsoft-graph-client');
 require('isomorphic-fetch');
+
+const RESOURCE_ID = '0800cd2b-ec6f-4544-bfa4-047856e99e2d';
 
 module.exports = {
   getAuthenticatedClient,
   getEvents,
   getUserDetails,
+  getUserRole,
 };
-
-async function getUserDetails(accessToken) {
-  const client = getAuthenticatedClient(accessToken);
-
-  const user = await client.api('/me').get();
-  return user;
-}
-async function getEvents(accessToken) {
-  const client = getAuthenticatedClient(accessToken);
-
-  const events = await client
-    .api('/me/events')
-    .select('subject,organizer,start,end')
-    .orderby('createdDateTime DESC')
-    .get();
-
-  return events;
-}
 
 function getAuthenticatedClient(accessToken) {
   // Initialize Graph client
@@ -39,4 +24,37 @@ function getAuthenticatedClient(accessToken) {
   });
 
   return client;
+}
+
+async function getEvents(accessToken) {
+  const client = getAuthenticatedClient(accessToken);
+  const events = await client
+    .api('/me/events')
+    .select('subject,organizer,start,end')
+    .orderby('createdDateTime DESC')
+    .get();
+
+  return events;
+}
+
+async function getUserDetails(accessToken) {
+  const client = getAuthenticatedClient(accessToken);
+  const user = await client.api('/me').get();
+  return user;
+}
+
+async function getUserRole(accessToken) {
+  const client = getAuthenticatedClient(accessToken);
+  const appRoleAssignments = await client.api('/me/appRoleAssignments')
+    .filter(`resourceId eq ${RESOURCE_ID}`)
+    .top(1)
+    .get();
+  console.dir(appRoleAssignments, { depth: null });
+
+  const appRoleAssignment = appRoleAssignments.value[0];
+  switch(appRoleAssignment.appRoleId) {
+    case 'b6fe44f2-b1ef-4e19-a06e-240078407dcb': return 'admin';
+    case 'd1c2ade8-98f8-45fd-aa4a-6d06b947c66f': return 'reader';
+    default: return 'user';
+  }
 }
--- demo/graph-tutorial/routes/users.js
+++ demo/graph-tutorial/routes/users.js
@@ -3,6 +3,9 @@ const passport = require('passport');
 var express = require('express');
 var router = express.Router();
 
+const graph = require('../graph');
+const tokens = require('../tokens');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -25,8 +28,15 @@ function authenticate(opts={}) {
 // https://stackoverflow.com/questions/47793615/utilizing-state-customstate-with-passport-azure-ad
 // https://dev.to/dangolant/dynamic-auth-redirects-with-passportjs-380g
 
-router.get('/me', authenticate(), (req, res) => {
-  res.send('me!!!');
+router.get('/me', authenticate(), async (req, res, next) => {
+  try {
+    const accessToken = await tokens.getAccessToken(req);
+    const role = await graph.getUserRole(accessToken);
+    res.send({ role });
+  } catch(err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
